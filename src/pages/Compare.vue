7a2d4eb4c6c8b4f3896dd88010f3a23e9b5f5fcb
Kishan Nirghin <kishannirghin@Kishans-MacBook-Pro-2.local>
2020-12-04 14:57:30 +0100

    Move WASM to created lifecycle

--- src/pages/Compare.vue
+++ src/pages/Compare.vue
@@ -16,37 +16,36 @@ export default {
   data() {
     return {
       n: 10,
+      fib1: fibonacci,
+      fib2: undefined, // lazy-loaded by created lifecycle
     }
   },
-  methods: {
-    async fib () {
-      const imports = {
-        env: {
-          abort (msg, file, line, column) {
-            console.error({ msg, file, line, column })
-          }
+  created() {
+    const imports = {
+      env: {
+        abort (msg, file, line, column) {
+          console.error({ msg, file, line, column })
         }
       }
-
-      WebAssembly.instantiateStreaming(fetch('/build/optimized.wasm'), imports)
-      .then(results => {
-        console.log({x: results.instance.exports.fibonacci(this.n)});
-        console.log({y: this.jsfib(this.n)});
-      });
-    },
-
-    jsfib (n) {
-      if (n === 0){
-        return 0;
-      }
-      if (n === 1){
-        return 1;
-      }
-
-      return this.jsfib(n-1) + this.jsfib(n-2);
     }
 
+    WebAssembly.instantiateStreaming(fetch('/build/optimized.wasm'), imports)
+    .then(results => {
+      console.log({x: results.instance.exports.fibonacci(this.n)});
+      console.log({y: this.jsfib(this.n)});
+    });
   }
+}
+
+function fibonacci(n) {
+  if (n === 0){
+    return 0;
+  }
+  if (n === 1){
+    return 1;
+  }
+
+  return fibonacci(n-1) + fibonacci(n-2);
 }
 </script>
 
