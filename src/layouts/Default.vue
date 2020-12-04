f43cb732b1bdb92a482b5978354e5686602386c2
Jeroen Ooms <jooms@xebia.com>
2020-12-04 11:48:52 +0100

    Add tailwindcss

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+  <div>
+    <Hero />
   </div>
 </template>
+
+<script>
+import Hero from "~/components/Hero.vue";
+
+export default {
+  components: {
+    Hero,
+  },
+};
+</script>
 
 <static-query>
 query {
@@ -23,9 +24,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
