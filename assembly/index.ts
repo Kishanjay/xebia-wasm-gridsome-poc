0377441d66692eeca73651160a67b38628f34865
Kishan Nirghin <kishannirghin@Kishans-MacBook-Pro-2.local>
2020-12-04 14:22:56 +0100

    Add fibonacci function

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,15 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+// fibonacci sequence using recursion
+export function fibonacci(n: i32): i32 {
+  if (n === 0){
+    return 0;
+  }
+  if (n === 1){
+    return 1;
+  }
+
+  return fibonacci(n-1) + fibonacci(n-2);
+}
